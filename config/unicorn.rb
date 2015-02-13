291a45328230994f10f5b64f61e92d12e4841a90
Tommy Dang <quantumventuress@gmail.com>
2015-02-12 21:51:29 -0800

    updated shared_dir in unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
-app_dir    = "/var/www/quantum"
-shared_dir = "#{app_dir}/shared"
+app_dir    = File.expand_path "../../", __FILE__
+shared_dir = File.expand_path "../../../shared/", __FILE__
 
 working_directory app_dir
 
